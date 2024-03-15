7156855b97f38944c79bad704c88dcf44a5b813f
kbasiorka <kamil30042001@gmail.com>
2024-03-15 11:36:35 +0100

    Dodaj pliki projektów.

---
+++ clicker.cpp
@@ -0,0 +1,28 @@
+﻿// clicker.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Clicker_Model {
+
+
+};
+
+class Clicker_View {
+
+
+};
+class Clicker_Controller {
+
+
+};
+
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
